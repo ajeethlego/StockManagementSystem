935d499c627f1c25178e22aaf8ee34c7f8e604fb
Ajeeth N <ajeeth@whiteblue.in>
2023-08-28 16:37:29 +0530

    Fine Commit

--- Stock 4/Migrations/20230301074601_migration_test.Designer.cs
+++ Stock 4/Migrations/20230301074601_migration_test.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Stock4.DataT;
 
@@ -11,9 +12,10 @@ using Stock4.DataT;
 namespace Stock_4.Migrations
 {
     [DbContext(typeof(StockContext))]
-    partial class StockContextModelSnapshot : ModelSnapshot
+    [Migration("20230301074601_migration_test")]
+    partial class migration_test
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Stock 4/Migrations/20230301074601_migration_test.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Stock_4.Migrations
+{
+    public partial class migration_test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserWatchlist1_authorizedUsers_UserId",
+                table: "UserWatchlist1");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserWatchlist1_stockLists_StockId",
+                table: "UserWatchlist1");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserWatchlist1",
+                table: "UserWatchlist1");
+
+            migrationBuilder.RenameTable(
+                name: "UserWatchlist1",
+                newName: "userWatchlist1s");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserWatchlist1_UserId",
+                table: "userWatchlist1s",
+                newName: "IX_userWatchlist1s_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserWatchlist1_StockId",
+                table: "userWatchlist1s",
+                newName: "IX_userWatchlist1s_StockId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_userWatchlist1s",
+                table: "userWatchlist1s",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "API_HolidayLocal",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Occasion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<DateTime>(type: "Date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_API_HolidayLocal", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CalcAPI",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Qty = table.Column<int>(type: "int", nullable: false),
+                    Stockprice = table.Column<float>(type: "real", nullable: false),
+                    StampDuty = table.Column<int>(type: "int", nullable: false),
+                    Brokerage = table.Column<float>(type: "real", nullable: false),
+                    TotalPrice = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CalcAPI", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_userWatchlist1s_authorizedUsers_UserId",
+                table: "userWatchlist1s",
+                column: "UserId",
+                principalTable: "authorizedUsers",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_userWatchlist1s_stockLists_StockId",
+                table: "userWatchlist1s",
+                column: "StockId",
+                principalTable: "stockLists",
+                principalColumn: "StockId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_userWatchlist1s_authorizedUsers_UserId",
+                table: "userWatchlist1s");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_userWatchlist1s_stockLists_StockId",
+                table: "userWatchlist1s");
+
+            migrationBuilder.DropTable(
+                name: "API_HolidayLocal");
+
+            migrationBuilder.DropTable(
+                name: "CalcAPI");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_userWatchlist1s",
+                table: "userWatchlist1s");
+
+            migrationBuilder.RenameTable(
+                name: "userWatchlist1s",
+                newName: "UserWatchlist1");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_userWatchlist1s_UserId",
+                table: "UserWatchlist1",
+                newName: "IX_UserWatchlist1_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_userWatchlist1s_StockId",
+                table: "UserWatchlist1",
+                newName: "IX_UserWatchlist1_StockId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserWatchlist1",
+                table: "UserWatchlist1",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserWatchlist1_authorizedUsers_UserId",
+                table: "UserWatchlist1",
+                column: "UserId",
+                principalTable: "authorizedUsers",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserWatchlist1_stockLists_StockId",
+                table: "UserWatchlist1",
+                column: "StockId",
+                principalTable: "stockLists",
+                principalColumn: "StockId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
